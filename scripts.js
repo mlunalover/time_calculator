f9a183802664e2055cef5ad62cd3e44a683ec9fc
Matt Lunalover <mlunalover@gmail.com>
2015-07-22 15:09:21 -0400

    its time

---
+++ scripts.js
@@ -0,0 +1,11 @@
+var secondsPerMin = 60;
+var minsPerHour = 60;
+var hoursPerDay = 24;
+var daysPerWeek = 7;
+var weeksPerYear = 52;
+var secondsPerYear = 52;
+var secondsPerDay = secondsPerMin * minsPerHour * hoursPerDay;
+document.write('<p>There are ' + secondsPerDay + ' seconds in a day</p>');
+var yearsAlive = prompt('How old are you?');
+var yearsAliveSeconds = secondsPerDay * yearsAlive;
+document.write('<p>You have been alive ' + yearsAliveSeconds + ' seconds in my life</p>');
